5eb161e97c54e24a48f3fa9315016b8576881e2f
moluwatomipe-bit <moluwatomipe@gmail.com>
2026-02-17 12:02:11 -0500

    Create supabase.ts

---
+++ supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://yolnmuisgloideripwqt.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbG5tdWlzZ2xvaWRlcmlwd3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNDI4MzMsImV4cCI6MjA4NjkxODgzM30._cxWUqSea1xQpAEp-xstWO4V-oJeKj_Q6NFz4raV5no';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
